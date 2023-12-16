669d1989631cf4b3717b068466e05f3595551a80
Meri <voidmeridian@gmail.com>
2023-12-16 15:14:48 -0500

    added font2 patch

--- config.def.h
+++ config.def.h
@@ -5,7 +5,11 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "CaskaydiaCove NFM Bold:pixelsize=12:antialias=true:autohint=true";
+static char *font = "CaskaydiaCove NFM Bold:pixelsize=14:antialias=true:autohint=true";
+/* Spare fonts */
+static char *
+	font2[] = {
+		"CaskaydiaCove NFM Bold:pixelsize=14:antialias=true:autohint=true"};
 static int borderpx = 2;
 
 /*
